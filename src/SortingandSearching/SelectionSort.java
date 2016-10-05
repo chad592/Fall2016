23e2b004f473f6b195b84752c7f4351ca27fe3ff
chad592 <ashwinderchadha300@gmail.com>
2016-10-05 14:59:11 -0500

    Update SelectionSort.java

--- src/SortingandSearching/SelectionSort.java
+++ src/SortingandSearching/SelectionSort.java
@@ -9,6 +9,32 @@ package SortingandSearching;
  *
  * @author zulfikar
  */
-public class SelectionSort {
+
+    public class selection {
+    public static void main(String[] args)
+    {
+        int min,i,j, temp;
+        int a[]={5,8,11,14,4,7,1};
+         
+          for(i=0;i<a.length;i++)
+          {
+              min=i;
+              for( j=i+1;j<a.length;j++)
+                  if(a[j]<a[min])
+                  {
+                      min=j;
+                  }
+          temp= a[i];
+          a[i]=a[min];
+          a[min]=temp;
+          }
+          
+          	
+
+for (int n = 0; n < a.length; n++) {
+   System.out.print(a[n] + ", ");
+
+    }
     
+}
 }
